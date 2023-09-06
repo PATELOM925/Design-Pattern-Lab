7d3fa35bb4e0006ac8ccccb4c7ee0da2a2c18e03
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2023-09-06 10:00:35 +0530

    Add files via upload

---
+++ 2 Structural Design Pattern /1 Composite - Method/Composite/Computerparts.java
@@ -0,0 +1,59 @@
+package Composite;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface Component // For Both Of Leaf And Composite Element
+{
+    void showPrice();
+}
+
+class Leaf implements Component {
+    int Price;
+    String Name;
+
+    // Constructor
+    public Leaf(int Price, String Name) {
+        // super(); // Why????
+        this.Price = Price;
+        this.Name = Name;
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println(Name + " : " + Price);
+    }
+}
+
+class Composite implements Component {
+    String Name;
+    List<Component> components = new ArrayList<>();
+
+    // Constructor
+    public Composite(String Name) {
+        // super(); // Why????
+        this.Name = Name;
+    }
+
+
+    public void addComponent(Component com) {
+        components.add(com);
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println("---------------" + Name + "---------------");
+        for (Component c : components) {
+            c.showPrice(); // Will Print The Price Of Each Object
+            // If It Is Leaf Object Directly The Price Will Be Printed
+            // If It Is A Composite Object The It Will Further Traverse Into It
+        }
+    }
+}
+
+public class Computerparts {
+
+}
